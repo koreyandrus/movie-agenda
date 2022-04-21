f4a8b231390a8ab04a0637a5ab7303d1fe274dc9
Korey Andrus <korey86@gmail.com>
2022-04-21 15:05:01 -0500

    Update movie component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { ListPageComponent } from './list-page/list-page.component';
 import { SearchPageComponent } from './search-page/search-page.component';
 import { WhatsHotPageComponent } from './whats-hot-page/whats-hot-page.component';
+import { MovieComponent } from './movie/movie.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/search', pathMatch: 'full'},
   { path: 'search', component: SearchPageComponent },
   { path: 'list', component: ListPageComponent },
   { path: 'whatshot', component: WhatsHotPageComponent },
+  { path: 'movie', component: MovieComponent },
 ];
 
 @NgModule({
